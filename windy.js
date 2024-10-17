2012fdb165c4883f1d4442e90480acd1c524afa1
easonb-cloud <eason1.29@hotmail.com>
2024-10-17 22:38:01 +0800

    Update windy.js

--- windy.js
+++ windy.js
@@ -5,7 +5,7 @@
 
 
 [Script]
-windy=type=^https?:\/\/api\.windyapp\.co\/apiV9\.php(\?method=syncUserData&userID=\w+)?,requires-body=1,script-path=https://raw.githubusercontent.com/easonb-cloud/windyJs/refs/heads/main/windy.js
+windy=type=http-response,pattern=^https?:\/\/api\.windyapp\.co\/apiV9\.php(\?method=syncUserData&userID=\w+)?,requires-body=1,script-path=https://raw.githubusercontent.com/easonb-cloud/windyJs/refs/heads/main/windy.js
 
 
 [MITM] 
@@ -14,9 +14,11 @@ hostname = %APPEND% api.windyapp.co
 *******************************/
 
 
-var body = $response.body
-  .replace(/isPro":\d+/g, 'isPro":1')
-  .replace(/becameProTimestamp":\w+/g, 'becameProTimestamp":1628431359')
-  .replace(/proType":\w+/g, 'proType":1');
+var body = $response.body; // 獲取響應的主體
 
-$done({ body });
+// 假設這是要修改的 JSON 結構
+body = body.replace(/"isPro":\d+/g, '"isPro":1')
+           .replace(/"becameProTimestamp":\w+/g, '"becameProTimestamp":1628431359')
+           .replace(/"proType":\w+/g, '"proType":1');
+
+$done({ body }); // 返回修改後的響應
