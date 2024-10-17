df5e34dea3d3c41768207d34bf738548d2545f79
easonb-cloud <eason1.29@hotmail.com>
2024-10-17 12:37:48 +0800

    Update globalTidal.js

--- globalTidal.js
+++ globalTidal.js
@@ -1,9 +1,7 @@
 /******************************
 #!name=全球潮汐
 #!desc=全球潮汐
-#!homepage=https://github.com/deezertidal
-#!author=yqc007
-#!icon=https://raw.githubusercontent.com/deezertidal/private/main/icons/adguard.png
+
 
 
 [Script]
