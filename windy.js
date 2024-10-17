df955a6cdf90b21f93cd22f208b37e79cd31e801
easonb-cloud <eason1.29@hotmail.com>
2024-10-17 22:46:05 +0800

    Update windy.js

--- windy.js
+++ windy.js
@@ -18,7 +18,7 @@ var body = $response.body; // 獲取響應的主體
 
 // 假設這是要修改的 JSON 結構
 body = body.replace(/"isPro":\d+/g, '"isPro":1')
-           .replace(/"becameProTimestamp":\w+/g, '"becameProTimestamp":1628431359')
+           .replace(/"becameProTimestamp":\w+/g, '"becameProTimestamp":1728431359')
            .replace(/"proType":\w+/g, '"proType":1');
 
 $done({ body }); // 返回修改後的響應
